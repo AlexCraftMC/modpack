10bde99746176f15e041d756ef5a6c721ea37032
coderboy14 <manziethan11@gmail.com>
2021-11-26 01:38:25 -0500

    Added build system

--- scripts/build.js
+++ scripts/build.js
@@ -7,6 +7,8 @@
 // Libraries
 const fs = require('fs');
 const https = require('https');
+const request = require('request');
+const zip = require('node-zip');
 
 // Modpack json!
 const config = require('../modpack.json');
@@ -18,7 +20,7 @@ const forge_version = "36.2.0";
 
 
 // Utils
-function getForgeDownload(version) {
+function getForgeDownloadPath() {
     var version_string = `${minecraft_version}-${forge_version}`
     return `https://files.minecraftforge.net/maven/net/minecraftforge/forge/${version_string}/forge-${version_string}-universal.jar`
 }
@@ -41,25 +43,38 @@ function httpGET(url) {
 }
 
 function downloadFile(url, path) {
-    console.log(`Downloading @ "${url}"`)
+    const download = (url, dest, cb) => {
+        const file = fs.createWriteStream(dest);
+        const sendReq = request.get(url);
+
+        // verify response code
+        sendReq.on('response', (response) => {
+            if (response.statusCode !== 200) {
+                return cb('Response status was ' + response.statusCode);
+            }
+
+            sendReq.pipe(file);
+        });
+
+        // close() is async, call cb after close completes
+        file.on('finish', () => file.close(cb));
+
+        // check for request errors
+        sendReq.on('error', (err) => {
+            fs.unlink(dest);
+            return cb(err.message);
+        });
+
+        file.on('error', (err) => { // Handle errors
+            fs.unlink(dest); // Delete the file async. (But we don't check the result)
+            return cb(err.message);
+        });
+    };
 
     return new Promise((resolve, reject) => {
-        try {
-            https.get(url, (res) => {
-
-                console.log(res.headers);
-
-                const filePath = fs.createWriteStream(path);
-
-                res.pipe(filePath);
-                filePath.on('finish',() => {
-                    filePath.close();
-                    resolve();
-                });
-            });
-        } catch (err) {
-            reject(err);
-        }
+        download(url, path, () => {
+            resolve();
+        })
     });
 }
 
@@ -75,17 +90,11 @@ function init() {
     }
 }
 
-async function get_curse_file_url(projectId, fileId) {
-
-}
-
 async function install_curse_mod(projectId, fileId) {
-    // https://www.curseforge.com/minecraft/mc-mods/jei/download/3040523/file
     var file_url = await httpGET(`https://addons-ecs.forgesvc.net/api/v2/addon/${projectId}/file/${fileId}/download-url`);
-    
     var segments = file_url.split('/')
     var file_name = segments[segments.length - 1];
-
+    console.log(`Downloading mod "${file_name}"...`);
     await downloadFile(file_url, `${build_root}/mods/${file_name}`);
 }
 
@@ -99,15 +108,38 @@ async function install_mods() {
     config.mods.urls.forEach(url => {
         var segments = url.split('/')
         var file_name = segments[segments.length - 1];
+        console.log(`Downloading mod "${file_name}"...`);
         awaiting.push(downloadFile(url, `${build_root}/mods/${file_name}`));
     });
+
     await Promise.all(awaiting);
 }
 
+async function install_forge() {
+    var url = getForgeDownloadPath();
+    await downloadFile(url, `${build_root}/bin/modpack.jar`);
+}
+
+async function create_zip() {
+    return new Promise((resolve, reject) => {
+        var arch = zip();
+
+        arch.file(`${build_root}/build/bin`, `${build_root}/build/mods`);
+        fs.writeFileSync(`${build_root}/modpack.zip`, arch.generate({
+          base64: false,
+          compression: 'DEFLATE'  
+        }), 'binary', (err) => {
+            reject(err);
+        });
+        resolve();
+    });
+}
 
 // Main!
 (async () => {
     console.log("Building modpack...");
     init();
     await install_mods();
+    await install_forge();
+    // await create_zip();
 })()
